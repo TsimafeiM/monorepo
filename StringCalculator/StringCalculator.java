8531637da29910b092b724b6167fe77ca2bad66f
TsimafeiM <tsimafei.malashchytski@carbon-it.com>
2025-01-14 16:21:28 +0100

    string calculator

---
+++ StringCalculator/StringCalculator.java
@@ -0,0 +1,103 @@
+public class StringCalculator {
+
+    public static int Add(String input){
+        if(input.isBlank()){
+            return 0;
+        }
+        String delimiteur = getDelimiteur(input);
+        String normalizedInput = normalizeInput(input, !delimiteur.isBlank());
+        String[] splittedInput = normalizedInput.split(",");
+        negativeNumbersCheck(splittedInput, delimiteur);
+        return calculateNumbers(splittedInput, delimiteur);
+    }
+
+    protected static int calculateNumbers(String[] splittedInput, String delimiteur) {
+        int total = 0;
+        for(String splittedString : splittedInput){
+            //gestion du delimiteur custom
+                if(notNumeric(splittedString) && !delimiteur.isBlank()){
+                    String[] resplittedString = splittedString.split(delimiteur);
+                    total = addStringToTotal(resplittedString[0], total);
+                    total = addStringToTotal(resplittedString[1], total);
+                }else {
+                    total = addStringToTotal(splittedString, total);
+                }
+        }
+        return total;
+    }
+
+    private static int addStringToTotal(String splittedString, int total) {
+        int parsedInt = Integer.parseInt(splittedString);
+        if(parsedInt < 1000){
+            total += parsedInt;
+        }
+        return total;
+    }
+
+    protected static void negativeNumbersCheck(String[] splittedInput, String delimiteur) {
+        StringBuilder negativeNumbers = new StringBuilder();
+        int negativesCount = 0;
+        for(String splittedString : splittedInput){
+            //gestion du delimiteur custom
+            if(notNumeric(splittedString) && !delimiteur.isBlank()){
+                for(String resplittedString : splittedString.split(delimiteur)){
+                    negativesCount = handleNegatives(resplittedString, negativesCount, negativeNumbers);
+                }
+            }else {
+                negativesCount = handleNegatives(splittedString, negativesCount, negativeNumbers);
+            }
+        }
+        String exceptionText;
+        if(negativesCount > 0){
+            exceptionText = "Les nombres négatifs ne sont pas autorisés";
+            if(negativesCount > 1){
+                exceptionText = exceptionText + " : " + negativeNumbers;
+            }
+            throw new RuntimeException(exceptionText);
+        }
+    }
+
+    private static int handleNegatives(String resplittedString, int countNegatives, StringBuilder negativeNumbers) {
+        // "" donne 0
+        int parsedInt = Integer.parseInt(resplittedString);
+        if(parsedInt <= 0){
+            appendNegativeNumbersWithCommas(resplittedString, countNegatives, negativeNumbers);
+            countNegatives++;
+        }
+        return countNegatives;
+    }
+
+    private static void appendNegativeNumbersWithCommas(String resplittedString, int countNegatives, StringBuilder negativeNumbers) {
+        if(countNegatives == 0){
+            negativeNumbers.append(resplittedString);
+        }else {
+            negativeNumbers.append(",");
+            negativeNumbers.append(resplittedString);
+        }
+    }
+
+    private static boolean notNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return false;
+        } catch(NumberFormatException e){
+            return true;
+        }
+    }
+
+    private static String normalizeInput(String input, boolean hasDelimiter) {
+        if(hasDelimiter){
+            input = input.substring(3);
+        }
+        return input.replaceAll("\n", ",");
+    }
+
+    protected static String getDelimiteur(String input) {
+        int indexOfDelim = input.indexOf("\\");
+        String delimiteur = "";
+        if (indexOfDelim != -1){
+            delimiteur = input.substring(indexOfDelim+1,indexOfDelim+2);
+        }
+        return delimiteur;
+    }
+}
---
+++ StringCalculator/StringCalculatorTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void Test012Sum() {
+        Assertions.assertEquals(0, StringCalculator.Add(""));
+        Assertions.assertEquals(1, StringCalculator.Add("1"));
+        Assertions.assertEquals(3, StringCalculator.Add("1,2"));
+    }
+
+    @Test
+    public void Test012345Sum() {
+        Assertions.assertEquals(15, StringCalculator.Add("1,2,3,4,5"));
+    }
+
+    @Test
+    public void TestSautSum() {
+        Assertions.assertEquals(6, StringCalculator.Add("1\n2,3"));
+    }
+
+    @Test
+    public void TestGetDelimiteur() {
+        Assertions.assertEquals(";", StringCalculator.getDelimiteur("\\;\n1,2;3"));
+    }
+
+    @Test
+    public void TestDelimiteurPerso() {
+        Assertions.assertEquals(6, StringCalculator.Add("\\;\n1,2;3"));
+    }
+
+    @Test
+    public void TestNombresNegatifs() {
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> StringCalculator.Add("-5,2,-10,9"));
+        Assertions.assertEquals("Les nombres négatifs ne sont pas autorisés : -5,-10", runtimeException.getMessage());
+    }
+
+    @Test
+    public void TestNombresNegatifsWithDelimteur() {
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> StringCalculator.Add("\\;,-5,2;-10,9"));
+        Assertions.assertEquals("Les nombres négatifs ne sont pas autorisés : -5,-10", runtimeException.getMessage());
+    }
+
+    @Test
+    public void TestIgnoreOver1000() {
+        Assertions.assertEquals(6, StringCalculator.Add("1,2,3,1000"));
+    }
+}
